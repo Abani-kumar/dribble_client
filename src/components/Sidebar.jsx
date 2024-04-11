2f92b00bf837999c362f1562824a29b2725e5e04
Abani-kumar <abanikuma073@gmail.com>
2024-04-11 21:23:00 +0530

    sidebar issue

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = ({open,setSidebaropen}) => {
   const { user } = useSelector((state) => state.auth);
 
   return (
-    <div className={` absolute top-0 left-0 bottom-0 transform transition-transform duration-500 ease-in-out ${open ?'md:translate-x-0  right-12' : '-translate-x-full md:translate-x-0'  } overflow-hidden bg-slate-300 p-4 `}>
+    <div className={` absolute top-0 left-0 bottom-0 transform transition-transform duration-500 ease-in-out ${open && 'right-12'  } overflow-hidden bg-slate-300 p-4 `}>
       <nav className=' flex flex-col gap-5 ml-2'>
         <div className=' flex  justify-between'>
             <div className=' flex flex-col gap-2 '>
